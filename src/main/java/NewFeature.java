2ac7979311e57168801b70e083515f0ed19b8449
Nikita <nikita258963@gmail.com>
2020-10-24 21:39:02 +0300

    trying to create conflict on master

--- src/main/java/NewFeature.java
+++ src/main/java/NewFeature.java
@@ -2,5 +2,8 @@ public class NewFeature {
     public static void someMethod(){
         System.out.println("New feature in feature branc ");
         System.out.println("problem2git ");
+        // new feature will be here
+        for (int i= 0;;){
+        }
     }
 }
