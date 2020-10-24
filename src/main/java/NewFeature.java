abc43fa82211ee041da7caabea89f962c20f1dda
Nikita <nikita258963@gmail.com>
2020-10-24 21:24:14 +0300

    222

--- src/main/java/NewFeature.java
+++ src/main/java/NewFeature.java
@@ -1,5 +1,6 @@
 public class NewFeature {
     public static void someMethod(){
-        System.out.println("New feature in feature branch");
+        System.out.println("New feature in feature branc ");
+        System.out.println("additional branch for feature  ");
     }
 }
